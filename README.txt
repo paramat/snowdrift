807c41dfa53d5527cbd9c21462a3db99163fd619
paramat <mat.gregory@virginmedia.com>
2014-01-20 08:21:59 +0000

    Temp/humidity and snow mod modes. Snow and rain independant and optional

--- README.txt
+++ README.txt
@@ -1,29 +1,4 @@
-Snowdrift 0.2.0 by paramat
+Snowdrift 0.2.1 by paramat
 For latest stable Minetest and back to 0.4.6
 Depends default
 Licenses: code WTFPL, textures CC BY-SA
-
-Version 0.1.0
----------------
-* Snowfall only in the snow biomes of snow mod by Splizard. You will need to disable snow mod snowfall.
-* Minimal processing for less powerful computers.
-* Snowfall is only seen by a player if the player is outside under open sky.
-* Each snowflake has it's own random trajectory, initial position, initial velocity and acceleration.
-* Snow falls at an average of 1m/s.
-* Snow drifts southwards at an average of 1m/s, a slightly lower speed than the southward cloud drift.
-* Snowflakes are sized to match 16x16 texture resolution and the icons used in my texture pack.
-* The snow box around the player is 128x128x32 nodes in size.
-* Parameter PROCHA controls snow density and processing load, default is 0.5, try 1 for denser snow.
-
-Version 0.1.1
--------------
-* Mod is too light, doubled maximum snow density, added second snowflake design with a square symmetry.
-* Snow density is now smoothly varied by how deep into a snow biome a player is.
-
-Version 0.1.2
--------------
-* Maximum snowfall doubled, 4 snowflake designs.
-
-v0.2.0
-------
-* Rain defined by a second perlin noise and threshold.
--- init.lua
+++ init.lua
@@ -1,25 +1,30 @@
--- Snowdrift 0.2.0 by paramat
+-- snowdrift 0.2.1 by paramat
 -- For latest stable Minetest and back to 0.4.6
 -- Depends default
--- Licenses: Code WTFPL. Textures CC BY-SA.
--- This is intended to be used as alternative snowfall for the snow mod by Splizard.
--- The code is partly derived from weather mod by Jeija and snow mod version 1.8 by Splizard.
+-- Licenses: code WTFPL, textures CC BY-SA
 
 -- Parameters
 
-local PROCHA = 0.5 -- (0 to 1) -- Processing chance
+local PROCHA = 0.2 -- (0 to 1) -- Per player processing chance, randomizes and spreads processing load
+local DROPS = 8 -- Rainfall heaviness
+local SNOWV6 = true -- Snowfall in snow biomes of snow mod by Splizard
+local RAIN = false -- Rain above humidity threshold
+local THOVER = false -- Instea use a temperature and humidity system with
+			-- snow in overlap of cold and humid
+			-- else rain in humid areas
+local SEEDT = 112 -- 112 -- If THOVER = true, set these to your mapgen's temperature noise parameters
+local OCTAT = 3	 -- 3
+local PERST = 0.5 -- 0.5
+local SCALT = 150 -- 150
 
-local SEEDS = 112 -- 112 -- Snow mod default biome noise parameters
-local OCTAS = 3 -- 3
-local PERSS = 0.5 -- 0.5
-local SCALS = 64 -- 150
-local SNOWT = 0.4 -- 0.53
+local TET = -0.53 -- -0.53 -- Temperature threshold for snow
+				-- If SNOWV6 = true set to -0.53
+local SEEDH = 72384 -- 72384 -- Set these to your mapgen's humidity noise parameters
+local OCTAH = 4 -- 4
+local PERSH = 0.66 -- 0.66
+local SCALH = 500 -- 500
 
-local SEEDR = -90000667 
-local OCTAR = 3
-local PERSR = 0.5
-local SCALR = 64
-local RAINT = 0.4
+local HUT = 0.5 -- 0.5 -- Humidity threshold for rain
 
 -- Stuff
 
@@ -28,76 +33,104 @@ snowdrift = {}
 -- Globalstep function
 
 minetest.register_globalstep(function(dtime)
-	-- if not raining at this time then return
+	-- TODO if not raining at this time then return
 	for _, player in ipairs(minetest.get_connected_players()) do
 		if math.random() > PROCHA then
 			return
 		end
 		local ppos = player:getpos()
-		if minetest.env:get_node_light(ppos, 0.5) ~= 15 then
+		if minetest.get_node_light(ppos, 0.5) ~= 15 then
 			return
 		end
-		local perlins = minetest.env:get_perlin(SEEDS, OCTAS, PERSS, SCALS)
-		local noises = perlins:get2d({x = ppos.x, y = ppos.z})
-		if noises > SNOWT then -- if snow biome
-			if math.random() < noises - SNOWT then
-				minetest.add_particle(
-					{x = ppos.x - 64 + math.random(0, 128), y= ppos.y + 16, z= ppos.z - 48 + math.random(0, 128)}, -- posi
-					{x = math.random() / 5 - 0.1, y = math.random() / 5 - 1.1, z = math.random() / 5 - 1.1}, -- velo
-					{x = math.random() / 50 - 0.01, y = math.random() / 50 - 0.01, z = math.random() / 50 - 0.01}, -- acce
-					32,
-					2.8,
-					false,
-					"snowdrift_snowflake1.png",
-					player:get_player_name()
-				)
-				minetest.add_particle(
-					{x = ppos.x - 64 + math.random(0, 128), y= ppos.y + 16, z= ppos.z - 48 + math.random(0, 128)}, -- posi
-					{x = math.random() / 5 - 0.1, y = math.random() / 5 - 1.1, z = math.random() / 5 - 1.1}, -- velo
-					{x = math.random() / 50 - 0.01, y = math.random() / 50 - 0.01, z = math.random() / 50 - 0.01}, -- acce
-					32,
-					2.8,
-					false,
-					"snowdrift_snowflake2.png",
-					player:get_player_name()
-				)
-				minetest.add_particle(
-					{x = ppos.x - 64 + math.random(0, 128), y= ppos.y + 16, z= ppos.z - 48 + math.random(0, 128)}, -- posi
-					{x = math.random() / 5 - 0.1, y = math.random() / 5 - 1.1, z = math.random() / 5 - 1.1}, -- velo
-					{x = math.random() / 50 - 0.01, y = math.random() / 50 - 0.01, z = math.random() / 50 - 0.01}, -- acce
-					32,
-					2.8,
-					false,
-					"snowdrift_snowflake3.png",
-					player:get_player_name()
-				)
-				minetest.add_particle(
-					{x = ppos.x - 64 + math.random(0, 128), y= ppos.y + 16, z= ppos.z - 48 + math.random(0, 128)}, -- posi
-					{x = math.random() / 5 - 0.1, y = math.random() / 5 - 1.1, z = math.random() / 5 - 1.1}, -- velo
-					{x = math.random() / 50 - 0.01, y = math.random() / 50 - 0.01, z = math.random() / 50 - 0.01}, -- acce
-					32,
-					2.8,
-					false,
-					"snowdrift_snowflake4.png",
-					player:get_player_name()
-				)
+		local snow = false
+		local rain = false
+		local noiset
+		local noiseh
+		if SNOWV6 or THOVER then
+			local perlint = minetest.get_perlin(SEEDT, OCTAT, PERST, SCALT)
+			noiset = perlint:get2d({x = ppos.x + 150, y = ppos.z + 50})
+		end
+		if RAIN or THOVER then	
+			local perlinh = minetest.get_perlin(SEEDH, OCTAH, PERSH, SCALH)
+			noiseh = perlinh:get2d({x = ppos.x, y = ppos.z})
+		end
+		if THOVER then
+			if noiset < TET and noiseh > HUT then
+				snow = true
+			elseif noiseh > HUT then
+				rain = true
 			end
-		else -- check rain noise
-			local perlinr = minetest.get_perlin(SEEDR, OCTAR, PERSR, SCALR)
-			local noiser = perlinr:get2d({x = ppos.x, y = ppos.z})
-			if math.random() < noiser - RAINT then
-				for drop = 1, 4 do
-					minetest.add_particle(
-						{x = ppos.x - 16 + math.random(0, 32), y= ppos.y + 16, z= ppos.z - 16 + math.random(0, 32)}, -- posi
-						{x = 0, y = -8, z = -1}, -- velo
-						{x = 0, y = 0, z = 0}, -- acce
-						4,
-						2.8,
-						false,
-						"snowdrift_raindrop.png",
-						player:get_player_name()
-					)
+		elseif SNOWV6 then
+			if -noiset < TET then -- negative sign because snow mod noise is 'coldness'
+				snow = true
+			elseif RAIN then
+				if noiseh > HUT then
+					rain = true
 				end
+			end
+		elseif RAIN then
+			if noiseh > HUT then
+				rain = true
+			end
+		end
+		if snow then
+			minetest.add_particle(
+				{x = ppos.x - 64 + math.random(0, 128), y = ppos.y + 16, z = ppos.z - 48 + math.random(0, 128)}, -- posi
+				{x = math.random() / 5 - 0.1, y = math.random() / 5 - 1.1, z = math.random() / 5 - 1.1}, -- velo
+				{x = math.random() / 50 - 0.01, y = math.random() / 50 - 0.01, z = math.random() / 50 - 0.01}, -- acce
+				32,
+				2.8,
+				false,
+				"snowdrift_snowflake1.png",
+				player:get_player_name()
+			)
+			minetest.add_particle(
+				{x = ppos.x - 64 + math.random(0, 128), y = ppos.y + 16, z = ppos.z - 48 + math.random(0, 128)}, -- posi
+				{x = math.random() / 5 - 0.1, y = math.random() / 5 - 1.1, z = math.random() / 5 - 1.1}, -- velo
+				{x = math.random() / 50 - 0.01, y = math.random() / 50 - 0.01, z = math.random() / 50 - 0.01}, -- acce
+				32,
+				2.8,
+				false,
+				"snowdrift_snowflake2.png",
+				player:get_player_name()
+			)
+			minetest.add_particle(
+				{x = ppos.x - 64 + math.random(0, 128), y = ppos.y + 16, z = ppos.z - 48 + math.random(0, 128)}, -- posi
+				{x = math.random() / 5 - 0.1, y = math.random() / 5 - 1.1, z = math.random() / 5 - 1.1}, -- velo
+				{x = math.random() / 50 - 0.01, y = math.random() / 50 - 0.01, z = math.random() / 50 - 0.01}, -- acce
+				32,
+				2.8,
+				false,
+				"snowdrift_snowflake3.png",
+				player:get_player_name()
+			)
+			minetest.add_particle(
+				{x = ppos.x - 64 + math.random(0, 128), y = ppos.y + 16, z = ppos.z - 48 + math.random(0, 128)}, -- posi
+				{x = math.random() / 5 - 0.1, y = math.random() / 5 - 1.1, z = math.random() / 5 - 1.1}, -- velo
+				{x = math.random() / 50 - 0.01, y = math.random() / 50 - 0.01, z = math.random() / 50 - 0.01}, -- acce
+				32,
+				2.8,
+				false,
+				"snowdrift_snowflake4.png",
+				player:get_player_name()
+			)
+		end
+		if rain then
+			for drop = 1, DROPS do
+				minetest.add_particle(
+					{
+						x = ppos.x - 16 + math.random(0, 32),
+						y = ppos.y + 16,
+						z = ppos.z - 16 + math.random(0, 32)
+					}, -- posi
+					{x = 0, y = -8, z = -1}, -- velo
+					{x = 0, y = 0, z = 0}, -- acce
+					4,
+					2.8,
+					false,
+					"snowdrift_raindrop.png",
+					player:get_player_name()
+				)
 			end
 		end			
 	end
